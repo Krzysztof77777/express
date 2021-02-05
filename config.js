7c5eba66ed7e838cb3b78056abdf5b6e7dc4629e
Krzysztof Sluzewski <krzysztofsluzewski@gmail.com>
2021-02-05 12:14:34 +0100

    admin login, admin news

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    db: 'mongodb+srv://admin:3JXiMofFYtKNAWnV@cluster0.wjokd.mongodb.net/test?retryWrites=true&w=majority',
+    keySession: ['TWOJKLUCZ'],
+    maxAgeSession: 24 * 60 * 60 * 1000,
+}
